884671782afa75e0b02112bf69ab232fbcebe4cd
Sjors van Holst <sjorsvanholst@gmail.com>
2025-08-22 15:34:09 +0200

    ✨ Started assessment

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import "./globals.css";
+
+const rubik = Rubik({
+  variable: "--font-rubik",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DIKS",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${rubik.variable}`}>{children}</body>
+    </html>
+  );
+}
